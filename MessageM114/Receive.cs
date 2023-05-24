8a38018f3dd4404f4c3116a1ad83600bd8c1c673
Jejebal <96423022+Jejebal@users.noreply.github.com>
2023-05-24 15:14:03 +0200

    Receive méthode

---
+++ MessageM114/Receive.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageM114
+{
+    internal class Receive
+    {
+
+        IPEndPoint iPEndPoint;
+
+        public async void creatIPEndpoint(string ip)
+        {
+
+            if(iPEndPoint != null)
+            {
+
+                IPHostEntry iPHostInfo = await Dns.GetHostEntryAsync(ip);
+                IPAddress iPAdress = iPHostInfo.AddressList[0];
+
+                iPEndPoint = new (iPAdress, 15245);
+
+            }
+
+        }
+
+        public async Task<string> creatTcpListener()
+        {
+
+            TcpListener listener = new(iPEndPoint);
+
+            try
+            {
+
+                listener.Start();
+
+                using TcpClient handler = await listener.AcceptTcpClientAsync();
+                await handler.ConnectAsync(iPEndPoint);
+                await using NetworkStream stream = handler.GetStream();
+
+                byte[] buffer = new byte[10000];
+                int received = await stream.ReadAsync(buffer);
+
+                string message = Encoding.UTF8.GetString(buffer, 0, received);
+
+                return message;
+
+            }
+            finally
+            {
+
+                listener.Stop();
+
+            }
+
+        }
+
+    }
+
+}
---
+++ MessageM114/ReceiveForm.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Project      : Message M114
+ * File         : ReceiveForm.cs
+ * Description  : A programm that receives and send encrypted messages, this main form
+ * Authors      : Ballanfat Jeremy, Weber Jamie
+ * Date         : 24 May 2023
+**/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageM114
+{
+    public partial class ReceiveForm : Form
+    {
+        public ReceiveForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
