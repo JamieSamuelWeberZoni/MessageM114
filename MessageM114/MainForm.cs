0aff5114bc409d4a99aeaa039b4898ebd8640140
JamieSamuelWeberZoni <jamie.wbr@eduge.ch>
2023-05-24 13:29:05 +0200

    Initial commit

---
+++ MessageM114/MainForm.cs
@@ -0,0 +1,22 @@
+/**
+ * Project      : Message M114
+ * File         : MainForm.cs
+ * Description  : A programm that receives and send encrypted messages, this main form
+ * Authors      : Ballanfat Jeremy, Weber Jamie
+ * Date         : 24 May 2023
+**/
+namespace MessageM114
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
